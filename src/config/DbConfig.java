7fe8e16cd97260a01cd09993b436a34dcce13f05
Vinit Kumar Arora <vinitkumar2105@gmail.com>
2025-08-03 16:06:44 +0530

    Update DbConfig.java

--- src/config/DbConfig.java
+++ src/config/DbConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/garage_sys_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root123";
+    private static final String PASSWORD = "your_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
